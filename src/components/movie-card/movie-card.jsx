bb6785181e38ac5b2de8ffa5eba8627ffa1ec6d4
ediesalasmiller <98842098+ediesalasmiller@users.noreply.github.com>
2022-05-13 09:37:11 -0700

    Colums

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -6,17 +6,17 @@ import Card from "react-bootstrap/Card";
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
-    return;
-    <Card>
-      <Card.Img variant="top" src={movie.ImagePath} />
-      <Card.Body>
-        <Card.Title>{movie.Title}</Card.Title>
-        <Card.Text>{movie.Description}</Card.Text>
-        <Button onClick={() => onMovieClick(movie)} variant="link">
-          Opeen
-        </Button>
-      </Card.Body>
-    </Card>;
-    //<div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>
+    return (
+      <Card>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} variant="link">
+            Open
+          </Button>
+        </Card.Body>
+      </Card>
+    );
   }
 }
